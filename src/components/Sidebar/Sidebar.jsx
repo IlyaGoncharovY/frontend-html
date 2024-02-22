ac5fe8e095bd96406cc87fccc94bdbb68891719a
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-02-22 17:39:28 +0300

    fix(Sidebar) use data in another directory and hook

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,31 +1,14 @@
 import './sidebar.scss';
-import {useCallback, useState} from 'react';
 
+import {useSideBar} from "./Hook/useSideBar.js";
 import {Header} from "./HeaderSideBar/Header.jsx";
 import {generateContainerClassnames} from "../../assets";
 import {PagesContainer} from "./Pages/PagesContainer.jsx";
-
-const routes = [
-    {title: 'Home', icon: 'fas-solid fa-house', path: '/'},
-    {title: 'Sales', icon: 'chart-line', path: '/sales'},
-    {title: 'Costs', icon: 'chart-column', path: '/costs'},
-    {title: 'Payments', icon: 'wallet', path: '/payments'},
-    {title: 'Finances', icon: 'chart-pie', path: '/finances'},
-    {title: 'Messages', icon: 'envelope', path: '/messages'},
-];
-
-const bottomRoutes = [
-    {title: 'Settings', icon: 'sliders', path: '/settings'},
-    {title: 'Support', icon: 'phone-volume', path: '/support'},
-];
+import {bottomRoutes, routes} from "../../common";
 
 export const Sidebar = () => {
 
-    const [isOpened, setIsOpen] = useState(true)
-
-    const toggleSidebar = useCallback(() => {
-        setIsOpen(prevState => !prevState);
-    }, []);
+    const {isOpened, toggleSidebar} = useSideBar();
 
     const containerClassnames = generateContainerClassnames('sidebar', isOpened);
 

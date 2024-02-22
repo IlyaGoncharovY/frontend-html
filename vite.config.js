58e0aebdc10b3982d59cff17946d6acf3149860a
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-02-22 16:35:00 +0300

    feat(docker)

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/frontend-html',
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 })
